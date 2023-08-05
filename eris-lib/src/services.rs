d6006b787715e765bccc83e66961c441886089cd
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-08-05 07:56:46 -0700

    Rename from inbox->endpoint, outbox->client

--- eris-lib/src/services.rs
+++ eris-lib/src/services.rs
@@ -1,5 +1,4 @@
-/// A service which receives POST requests made by Discord.
-pub mod discord_inbox;
-
 /// A service which makes calls to the Discord API.
-pub mod discord_outbox;
+pub mod discord_client;
+/// A service which receives POST requests made by Discord.
+pub mod discord_endpoint;
