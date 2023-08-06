9a87d835de38473a15fe9397ed19bfaa5039fc11
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-08-06 16:35:20 -0700

    Simplify default_if_none

--- eris-lib/src/layers/default_if_none.rs
+++ eris-lib/src/layers/default_if_none.rs
@@ -1,46 +1,18 @@
-use futures_util::{FutureExt, future::{ready, Either, Ready}};
-use tower::{Layer, Service};
+use futures_util::{future::ready, FutureExt};
+use tower::{service_fn, Service};
 
-/// A layer which turns a Service that takes a Request and returns a 
+/// A layer_fn which turns a Service that takes a Request and returns a
 /// Default-able Response into a service which takes an Option and
 /// returns the default if the request is None.
-pub struct DefaultIfNoneLayer;
-
-impl<S> Layer<S> for DefaultIfNoneLayer {
-    type Service = DefaultIfNoneService<S>;
-
-    fn layer(&self, inner: S) -> Self::Service {
-        DefaultIfNoneService { inner }
-    }
-}
-
-/// A service that returns a default response if passed None as
-/// the request, or runs an inner service on the Some value.
-pub struct DefaultIfNoneService<S> {
-    inner: S,
-}
-
-impl<S, Request> Service<Option<Request>> for DefaultIfNoneService<S>
+pub fn default_if_none_layer_fn<S, R>(
+    mut service: S,
+) -> impl Service<Option<R>, Response = S::Response, Error = S::Error>
 where
-    S: Service<Request>,
+    S: Service<R>,
     S::Response: Default,
 {
-    type Response = S::Response;
-    type Error = S::Error;
-    type Future = Either<S::Future, Ready<Result<S::Response, S::Error>>>;
-
-    fn poll_ready(
-        &mut self,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), Self::Error>> {
-        self.inner.poll_ready(cx)
-    }
-
-    fn call(&mut self, req: Option<Request>) -> Self::Future {
-        if let Some(req) = req {
-            self.inner.call(req).left_future()
-        } else {
-            ready(Ok(S::Response::default())).right_future()
-        }
-    }
+    service_fn(move |request: Option<R>| match request {
+        Some(request) => service.call(request).left_future(),
+        None => ready(Ok(S::Response::default())).right_future(),
+    })
 }
