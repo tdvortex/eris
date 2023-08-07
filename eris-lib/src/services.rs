0f8c93c7d049b0ed42c9c1afad7afbb0293c8dc6
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-08-07 14:40:40 -0700

    Delete empty file

--- eris-lib/src/services.rs
+++ eris-lib/src/services.rs
@@ -2,11 +2,6 @@
 /// fowards them onto a handler service, and immediately responds with
 /// DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE to prevent timeouts.
 pub mod discord_endpoint;
-
-/// A service which receives a [DiscordClientAction] as a request
-/// and returns () as a response if successful.
-pub mod discord_client_action_service;
-
 /// A service which responds to an Interaction by queuing it and responding
 /// with DEFERRED_CHANNEL_MESSAGE as quickly as possible.
 pub mod interaction_response;
