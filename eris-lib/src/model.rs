d172d3a31965eea72dc6199018c3e62b696b9efd
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-08-11 05:00:10 -0700

    Data model placeholders

--- eris-lib/src/model.rs
+++ eris-lib/src/model.rs
@@ -1,1 +1,37 @@
+/// The base entity and actor in the system.
+pub mod application;
 
+/// The Block Activity.
+pub mod block;
+
+/// A Discord channel, which is also an Actor
+/// in ActivityPub so that it can follow others.
+pub mod channel;
+
+/// The Follow Activity.
+pub mod follow;
+
+/// Any Actor from an external source.
+pub mod foreign_actor;
+
+/// An Image object, mostly used as an attachment.
+pub mod image;
+
+/// The Like activity.
+pub mod like;
+
+/// A Discord message. This is **not** ActivityPub
+/// but does need an internal data representation.\
+pub mod message;
+
+/// A post on the network.
+pub mod post;
+
+/// A tombstone marker for a deleted entity.
+pub mod tombstone;
+
+/// A human app user. One-to-one with a Discord user.
+pub mod user;
+
+/// A Video object, mostly used as an attachment.
+pub mod video;
